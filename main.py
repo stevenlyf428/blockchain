09b8f1f94ee4d13bd1f16b8fc7b17681ecdadcef
stevenlyf428 <739218900@qq.com>
2022-05-12 01:31:05 +0800

    c4-添加交易类

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import datetime
+
+
 class Student:
-    Name = ''
+    Name = ""
     Credits = 20.0
-    def __init__(self,name=''):
+
+    def __init__(self, name=""):
         self.Name = name
 
 
-StuA = Student('A')
-print('Student: %s has %f credits' % (StuA.Name,StuA.Credits))
-StuB = Student('B')
-print('Student: %s has %f credits' % (StuB.Name,StuB.Credits))
+class Transaction:
+    def __init__(self, sender, recipient, value):
+        if sender.Credits > value:
+            sender.Credits -= value
+            recipient.Credits += value
+            self.time = datetime.datetime.now()
+            print("Transaction finished at %s" % (self.time))
+
+
+def main():
+    StuA = Student("A")
+    StuB = Student("B")
+    tran = Transaction(StuA, StuB, 10)
+
+    print("Student: %s has %f credits" % (StuA.Name, StuA.Credits))
+    print("Student: %s has %f credits" % (StuB.Name, StuB.Credits))
+
+
+if __name__ == "__main__":
+    main()
