a0490ff5b28d70ea78426c8dd706a3feadd67473
stevenlyf428 <739218900@qq.com>
2022-05-13 09:10:51 +0800

    c7-添加交易类for存储账户余额

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Crypto.PublicKey import RSA
 import os
 import hashlib
 
+import configparser  # 处理ini文件
+
 
 class Student:
     Name = ""
@@ -29,31 +31,32 @@ class Student:
         else:
             random = Crypto.Random.new().read
             # 生成私钥
-            self._private_key = RSA.generate(1024, random)  
-            self.export_Account(PrivateFile,self._private_key)
-            
+            self._private_key = RSA.generate(1024, random)
+            self.export_Account(PrivateFile, self._private_key)
 
         publickFile = "%s_Public.pem" % (stuName)
         if os.path.exists(publickFile):
             self._public_key = self.import_Account(publickFile)
         else:
             # 生成公钥
-            self._public_key = self._private_key.publickey() 
+            self._public_key = self._private_key.publickey()
             self.export_Account(self._public_key)
 
-        #计算出账户
-        str1 = binascii.hexlify(self._public_key.exportKey(format='DER')).decode('ascii')
-        str2 = hashlib.sha256(str1.encode('utf-8')).hexdigest()
-        str3 = hashlib.sha256(str2.encode('utf-8')).hexdigest()        
+        # 计算出账户
+        str1 = binascii.hexlify(self._public_key.exportKey(format="DER")).decode(
+            "ascii"
+        )
+        str2 = hashlib.sha256(str1.encode("utf-8")).hexdigest()
+        str3 = hashlib.sha256(str2.encode("utf-8")).hexdigest()
         self.Name = str3
         print("Account is ready: %s" % (self.Name))
-        
-    def export_Account(self, accountNamePemStr,key):
-        privatePemStr = key.exportKey()  
+
+    def export_Account(self, accountNamePemStr, key):
+        privatePemStr = key.exportKey()
         with open(accountNamePemStr, "wb") as f:
             f.write(privatePemStr)
             f.close()
-    
+
     def import_Account(self, key):
         with open(key, "rb") as f:
             publicPemStr = f.read()
@@ -62,20 +65,50 @@ class Student:
 
 
 class Transaction:
-    def __init__(self, sender, recipient, value):
+    iniFileName = "Account.ini"
+
+    def __init__(self):
+        return
+
+    def Transfer(self, sender, recipient, value):
+        sender.Credits = self.AccountGet(sender)
+        recipient.Credits = self.AccountGet(recipient)
         if sender.Credits > value:
             sender.Credits -= value
             recipient.Credits += value
             self.time = datetime.datetime.now()
             print("Transaction finished at %s" % (self.time))
-        else:
-            print("You need more money!Transaction failed!!")
+        self.AccountSave(sender)
+        self.AccountSave(recipient)
+        return
+
+    def AccountGet(self, Stu):  # 存储账户余额信息
+        cred = Stu.Credits
+        config = configparser.ConfigParser()
+        config.read(self.iniFileName, encoding="utf-8")
+        list = config.sections()  # 获取到配置文件中所有分组名称
+        if Stu.Name in list:  # 如果分组存在
+            cred = config.getfloat(Stu.Name, "Credits")
+        return cred
+
+    def AccountSave(self, Stu):  # 存储账户余额信息
+        config = configparser.ConfigParser()
+        config.read(self.iniFileName, encoding="utf-8")
+        list = config.sections()  # 获取到配置文件中所有分组名称
+        if Stu.Name not in list:  # 如果分组不存在
+            config.add_section(Stu.Name)  # 增加section
+        config.set(Stu.Name, "Credits", str(Stu.Credits))
+        with open(self.iniFileName, "w+") as f:
+            config.write(f)
+            f.close()
+        return
 
 
 def main():
     StuA = Student("A")
     StuB = Student("B")
-    tran = Transaction(StuA, StuB, 10)
+    tran = Transaction()
+    tran.Transfer(StuA, StuB, 10)
 
     print("Student: %s has %f credits" % (StuA.Name, StuA.Credits))
     print("Student: %s has %f credits" % (StuB.Name, StuB.Credits))
